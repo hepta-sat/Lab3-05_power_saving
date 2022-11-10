0b574f27b38c4c3651bd5c8a184e689ab5671c48
Charlie.com <csru22005@g.nihon-u.ac.jp>
2022-11-10 15:53:22 +0000

    changed to rawserial

--- main.cpp
+++ main.cpp
@@ -1,12 +1,25 @@
 #include "mbed.h"
-
-DigitalOut myled(LED1);
-
-int main() {
-  while(1) {
-    myled = 1;
-    wait(0.2);
-    myled = 0;
-    wait(0.2);
-  }
+#include "HEPTA_EPS.h"
+RawSerial pc(USBTX,USBRX,9600);
+HEPTA_EPS eps(p16,p26);
+int main(){
+    int flag = 0;
+    float btvol;
+    for(int i = 0; i < 100; i++) {
+        //Get Battery Voltage
+        eps.vol(&btvol);
+        pc.printf("BatVol = %.2f [V]\r\n",btvol);
+        //Power Saving Mode
+        if(btvol <= 3.5){
+            eps.shut_down_regulator();
+            flag = 1;
+        } else {
+            eps.turn_on_regulator();
+            flag = 0;
+        }
+        if(flag == 1){
+            pc.printf("Power saving mode ON\r\n");
+        }
+        wait(1.0);
+    }
 }
